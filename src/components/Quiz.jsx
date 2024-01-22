9ca7665fb60d0eadfa8f3375bb09b4dd3d6c8636
BMonesh <141898445+BMonesh@users.noreply.github.com>
2024-01-22 11:57:38 +0530

    Add files via upload

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,15 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import questions from "../questions";
 import "./Quiz.css";
 
+import Highlight from "../assets/Highlight.png"
+import DeHighlight from "../assets/De-Highlight.png"
+import kalviumLogo from "../assets/Kalvium-Logo-SVG.svg"
+
 const Quiz = () => {
-  const [darkMode, setDarkMode] = useState(false);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
+  const [highlited, setHighlited] = useState(false);
+
+  const [dark, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme ? savedTheme === "dark" : true;
+  });
 
   const toggleTheme = () => {
-    setDarkMode(!darkMode);
+    setTheme((prevTheme) => {
+      const newTheme = !prevTheme;
+      localStorage.setItem("theme", newTheme ? "dark" : "light");
+      return newTheme;
+    });
   };
+
+  useEffect(() => {
+    document.body.className = dark ? "dark-theme" : "light-theme";
+  }, [dark]);
 
   const handleAnswer = (isCorrect) => {
     if (isCorrect) {
@@ -25,23 +42,65 @@ const Quiz = () => {
     setScore(0);
   };
 
+  const headerStyle = {
+    backgroundColor: dark ? "#a9dbec" : "#00203f",
+    borderBottom: dark ? "4px solid #00203f" : "4px solid #a9dbec",
+  };
+
+  const headerButtonStyle = {
+    backgroundColor: dark ? "#00203f" : "#a9dbec",
+    color: dark ? "white" : "black",
+  };
+
+  const backStyle = {
+    backgroundColor: dark ? "#a9dbec" : "#00203f",
+  };
+
+  const mainStyle = {
+    backgroundColor: dark ? "#00203f" : "#a9dbec",
+  };
+
+  const optionStyle = {
+    backgroundColor: dark ? "#a9dbec" : "#00203f",
+    color: dark ? "black" : "white",
+  };
+
+  const paraStyle = {
+    color: dark ? "white" : "black",
+  };
+
+  const resultStyle = {
+    color: dark ? "#a9dbec" : "#00203f",
+  };
+
+  const resultBtn = {
+    backgroundColor: dark ? "#a9dbec" : "#00203f",
+    color: dark ? "black" : "white",
+  };
+
   return (
     <>
-      <div className={`app ${darkMode ? "dark" : "light"}`}>
-        <header>
-          <img src="" alt="Logo" />
-          <button id="toggle" onClick={toggleTheme}>
-            {darkMode === false ? "Light" : "Dark"}
+      <div className="app" style={backStyle}>
+        <header style={headerStyle}>
+          <img className="kalvium" src={kalviumLogo} alt="Logo" />
+          <button style={headerButtonStyle} id="toggle" onClick={toggleTheme}>
+            {dark === false ? "Light" : "Dark"}
           </button>
         </header>
         {currentQuestionIndex < questions.length ? (
-          <main>
+          <main style={mainStyle}>
+            <div className="high">
+              <img src={highlited == true ? `${Highlight}` : `${DeHighlight}`} onClick={() => {
+                highlited == true ? setHighlited(false) : setHighlited(true)
+              }}/>
+            </div>
             <div className="question">
-              <p>{questions[currentQuestionIndex].text}</p>
+              <p id={highlited ? "highlited" : ""} style={resultStyle}>{questions[currentQuestionIndex].text}</p>
               {questions[currentQuestionIndex].options.map((option) => (
                 <button
                   key={option.id}
                   onClick={() => handleAnswer(option.isCorrect)}
+                  style={optionStyle}
                 >
                   {option.text}
                 </button>
@@ -49,14 +108,16 @@ const Quiz = () => {
             </div>
           </main>
         ) : (
-          <main className="complete">
+          <main className="complete" style={mainStyle}>
             <div className="completed">
-              <h1>Result :)</h1>
-              <p>Quiz Completed!</p>
-              <p>
-                Your score is {score} out of {questions.length}.
+              <h1 style={paraStyle}>Result :)</h1>
+              <p style={resultStyle}>Quiz Completed!</p>
+              <p style={resultStyle}>
+                Your score is {score} out of {questions.length}
               </p>
-              <button onClick={restartQuiz}>Restart</button>
+              <button style={resultBtn} onClick={restartQuiz}>
+                Restart
+              </button>
             </div>
           </main>
         )}
